7c588a679324f142c0ca8c562e441a7e3f6bac0f
mrbmi513 <mrbmi513@gmail.com>
2014-04-29 21:39:10 -0500

    uses lists and for loops

--- powerball checker/powerballcheck.py
+++ powerball checker/powerballcheck.py
@@ -20,46 +20,31 @@ initial_your_numbers = input_your_numbers.read() #reads the text file
 winning_numbers = initial_winning_numbers.split(',') #splits the winning numbers into a list
 #now we will make sure our inputted numbers are actually integers. it will try what is in try, but if there is an error (or what python calls an exception), run the code under except
 try:
-    win1 = int(winning_numbers[0]) #remember lists start at 0!
-    win2 = int(winning_numbers[1])
-    win3 = int(winning_numbers[2])
-    win4 = int(winning_numbers[3])
-    win5 = int(winning_numbers[4])
-    winpowerball = int(winning_numbers[5])
+    sanitized_win = []
+    for number in winning_numbers:
+        tempwin = int(number)
+        sanitized_win.append(tempwin)
 except:
     print "There seems to be an error with the winning numbers file. Are they numbers?"
     raw_input("Press Enter to Exit")
     sys.exit()
 
 #this will be how we check our numbers
-def checknumbers(my1, my2, my3, my4, my5, mypowerball):
-    global win1
-    global win2
-    global win3
-    global win4
-    global win5
-    global winpowerball
-    global prize
-
-    mynumbers = [my1, my2, my3, my4, my5]
+def checknumbers(sanitized_nums):
+    global sanitized_win
     whiteballs = 0 #will count how many white balls we match
-    if mypowerball == winpowerball: #checks winningness of powerball
+    if sanitized_nums[5] == sanitized_win[5]: #checks winningness of powerball
         matchpowerball = True
         print "You matched the powerball"
     else:
         matchpowerball = False
         print "You did not match the powerball"
+    mynumbers = [sanitized_nums[0],sanitized_nums[1],sanitized_nums[2],sanitized_nums[3],sanitized_nums[4]]
+    winwhites = [sanitized_win[0],sanitized_win[1],sanitized_win[2],sanitized_win[3],sanitized_win[4]]
     for mynumber in mynumbers: #will check numbers for matches
-        if mynumber == win1:
-            whiteballs = whiteballs + 1
-        elif mynumber == win2:
-            whiteballs = whiteballs + 1
-        elif mynumber == win3:
-            whiteballs = whiteballs + 1
-        elif mynumber == win4:
-            whiteballs = whiteballs + 1
-        elif mynumber == win5:
-            whiteballs = whiteballs + 1
+        for winnumber in winwhites:
+            if mynumber == winnumber:
+                whiteballs = whiteballs + 1
     print "You matched",whiteballs,"White Balls"
     #check for prizes
     if matchpowerball == True:
@@ -100,25 +85,23 @@ for ticket in separate_tickets: #run this for each ticket in our list
     numbers = ticket.split(',') #separates the ticket's numbers
     print "Ticket",ticketcounter
     #this works the same as the try except above, pulling from the list of 'numbers'
-    try:
-        my1 = int(numbers[0])
-        my2 = int(numbers[1])
-        my3 = int(numbers[2])
-        my4 = int(numbers[3])
-        my5 = int(numbers[4])
-        mypowerball = int(numbers[5])
+   # try:
+    sanitized_nums = []
+    for number in numbers:
+        tempwin = int(number)
+        sanitized_nums.append(tempwin)
         #this sends the numbers to my check 'function' above and returns the prize amount
-        ticketprize = checknumbers(my1, my2, my3, my4, my5, mypowerball)
-        print "The prize for ticket",ticketcounter, "is $"+ticketprize
-    except:
-        print "There seems to be an error with ticket",ticketcounter,". Are they numbers?"
+    ticketprize = checknumbers(sanitized_nums)
+    print "The prize for ticket",ticketcounter, "is $"+ticketprize
+    #except:
+        #print "There seems to be an error with ticket",ticketcounter,". Are they numbers?"
     print ""
 
 #closes the files correctly
 input_winning_numbers.close()
 input_your_numbers.close()
 print ""
-print "Prizes differ in California. see powerball.com for CA prize info. This program and its developer are not related in any way to Powerball or the Multistate Lottery Association (MUSL)"
+print "Prizes differ in California. See powerball.com for CA prize info. This program and its developer are not related in any way to Powerball or the Multistate Lottery Association (MUSL). Numbers not official until verified."
 print ""
 raw_input("Press enter to exit")
 
